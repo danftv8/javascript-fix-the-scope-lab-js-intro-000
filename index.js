3d0c39b80b3c263ab6b1c1a093a4afb9e7c01342
Daniel Mullaney <danftv@gmail.com>
2021-01-11 22:43:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function add2(n) {
   // Feel free to move things around!
 }
 
-var funkyFunction = function() {
+var = funkyFunction() = function() {
   return function() {
     return "FUNKY!"
   }
@@ -30,5 +30,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()
-theFunk()
+var theFunk = funkyFunction()()
